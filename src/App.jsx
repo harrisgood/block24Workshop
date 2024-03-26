010b076cbfefbb7ba2b62dfa3e1f5feb777d204d
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-03-25 22:46:07 -0400

    first tab done, little more cleaning up the document

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(puppies)
 
   return (
     <>
-      
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+
     </>
   )
 }
